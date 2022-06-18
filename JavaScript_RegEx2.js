91ead91c29241e426ae2a834fc58622dd8b71647
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-06-18 14:02:06 +0200

    Add files via upload

--- JavaScript_RegEx2.js
+++ JavaScript_RegEx2.js
@@ -6,3 +6,23 @@ let result1 = humStr.match(huRegex);
 let result2 = hugStr.match(huRegex);
 console.log(result1, result2);
 
+let quoteSample = "Beware of bugs in the above code.";
+let vowelRegex = /[aeiouy]/ig; 
+let result = quoteSample.match(vowelRegex);
+console.log(result);
+
+let quote = "The quick brown fox jumped over the lazy dog.";
+let letterRange = /[a-z]/ig;
+let result3 = quote.match(letterRange);
+console.log(result3);
+
+//Search for a range of letters and numbers
+let quote2 = "Blueberry 3.141592653s are delicious!";
+let regex = /[h-s2-6]/ig;
+let result4 = quote2.match(regex); 
+console.log(result4);
+
+//Specify a set of characters you do not want to match
+let quote3 = "";
+
+//12:16
