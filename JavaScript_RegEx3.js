0f6dcc37e66c608530c5a2c0dfcb471379cbcfa2
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-06-19 19:01:50 +0200

    Add files via upload

--- JavaScript_RegEx3.js
+++ JavaScript_RegEx3.js
@@ -5,3 +5,16 @@ let res = str.match(regex);
 console.log(res);
 
 // 14:18 Match characters that occur zero or more times
+let str2 = "Goooooooooal!";
+let str3 = "gut feeling!";
+let str4 = "over the moon";
+let goRegex = /go*/gi;
+let res1 = str2.match(goRegex);
+let res2 = str3.match(goRegex);
+let res3 = str4.match(goRegex);
+console.log(res1, res2, res3);
+
+let chewieQuote ="Aaaaaaaaaaaaaaaaaarrrrgh!";
+let chewieRegex = /Aa*/;
+let chewieResult = chewieQuote.match(chewieRegex);
+console.log(chewieResult);
