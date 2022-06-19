ad9361b5ccd79be238980d19d799d19fa121d1ba
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-06-19 09:12:27 +0200

    Add files via upload

--- JavaScript_RegEx2.js
+++ JavaScript_RegEx2.js
@@ -23,6 +23,9 @@ let result4 = quote2.match(regex);
 console.log(result4);
 
 //Specify a set of characters you do not want to match
-let quote3 = "";
+let quote3 = "3 blind mice.";
+let myRegex = /[^0-9aeiouy]/ig; // match anything except numbers and vowels
+let result5 = quote3.match(myRegex);
+console.log(result5);
 
-//12:16
+//13:30
